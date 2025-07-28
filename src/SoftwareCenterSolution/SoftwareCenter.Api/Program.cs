e4b35dc5746817e7147bb37cbf5b69fd5a71e290
Jeffry Gonzalez <jeff@hypertheory.com>
2025-07-28 20:22:06 +0000

    basic post

--- src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Program.cs
@@ -18,7 +18,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapControllers(); // Go find all the controllers and look at the attributes (HttpGet, HttpPost, etc.)
+// and make yourself a cheat sheet.
+// if I get a POST /vendors - create a Vendors/Controller instance, and call the AddVendorAsync Method.
 Console.WriteLine("Fixing to run your API");
 app.Run(); // this is a "blocking method" basically a while(true) {... }
 Console.WriteLine("done running your API");
---
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/Vendors/Controller.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoftwareCenter.Api.Vendors;
+
+public class Controller : ControllerBase
+{
+    // this is the method you should call when a POST /vendors is received.
+    [HttpPost("/vendors")]
+    public async Task<ActionResult> AddAVendorAsync(
+        [FromBody] CreateVendorRequest request,
+        CancellationToken token)
+    {
+        // Mapping Code (copy from one object to another)
+        var response = new CreateVendorResponse(
+            Guid.NewGuid(),
+            request.Name,
+            request.Url,
+            request.PointOfContact
+            );
+        return Ok(response); 
+    }
+
+}
+
+/*{
+    "name": "Microsoft",
+    "url": "https://wwww.microsoft.com",
+    "pointOfContact": {
+        "name": "Satya",
+        "phone": "800 big-boss",
+        "email": "satya@microsoft.com"
+    }
+}*/
+
+
+public record CreateVendorRequest(
+    string Name, string Url, CreateVendorPointOfContactRequest PointOfContact);
+
+public record CreateVendorPointOfContactRequest(string Name, string Phone, string Email);
+
+
+public record CreateVendorResponse(
+    Guid Id,
+    string Name, string Url, CreateVendorPointOfContactRequest PointOfContact
+    );
