67d127f4a33a475f4a5e4127344ba6345be10714
Jeffry Gonzalez <jeff@hypertheory.com>
2025-07-28 16:03:53 +0000

    Auto-commit changes before creating step 02-converted-to-top-level

--- src/DotNetCatchUp/DotNetCatchUp/Program.cs
+++ src/DotNetCatchUp/DotNetCatchUp/Program.cs
@@ -1,10 +1,2 @@
-﻿namespace DotNetCatchUp
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+Console.WriteLine("Hello, World!");
