af5d9e4291932c85f2fc44736b64c7556d42a950
Jeffry Gonzalez <jeff@hypertheory.com>
2025-07-31 18:46:07 +0000

    :shipit:

--- src/SoftwareCenterSolution/SoftwareCenter.Api/CatalogItems/Api.cs
+++ src/SoftwareCenterSolution/SoftwareCenter.Api/CatalogItems/Api.cs
@@ -7,20 +7,28 @@ public static class Api
     public static IEndpointRouteBuilder MapCatalogItems(this IEndpointRouteBuilder app)
     {
 
-        app.MapPost("/vendors/{id:guid}/items", (
-            Guid Id, 
+        app.MapPost("/vendors/{id:guid}/items", async (
+            Guid id, 
             CatalogItemCreateRequest request, 
-            IDocumentSession session) =>
+            IDocumentSession session,
+            ILookupVendors vendorLookups) =>
         {
             // validate the stuff - see the issue.
             // create the entity
-
+            bool noSuchVendor = await vendorLookups.CheckIfVendorExistsAsync(id);
+            if(noSuchVendor)
+            {
+                return Results.NotFound();
+            }
             // from CatalogItemCreateRequest -> CatalogItemEntity
-            var entity = request.MapToEntity();
+            var entity = request.MapToEntity(id);
             // save it to the database (side effect)
+            session.Store(entity);
+            await session.SaveChangesAsync();
             // create a response 
+            var response = entity.MapToResponse();
 
-            return Results.Ok();
+            return Results.Ok(response);
         });
         return app;
     }
@@ -39,12 +47,13 @@ public record CatalogItemCreateRequest
     public string Description { get; set; } = string.Empty;
     public string Version { get; set; } = string.Empty;
 
-    public  CatalogItemEntity MapToEntity()
+    public  CatalogItemEntity MapToEntity(Guid vendorId)
     {
         return new CatalogItemEntity
         {
 
             Id = Guid.NewGuid(),
+            VendorId = vendorId,
             Created = DateTimeOffset.UtcNow,
             Description = Description,
             Name = Name,
@@ -56,17 +65,36 @@ public record CatalogItemCreateRequest
 public record CatalogItemDetails
 {
     public Guid Id { get; set; }
+
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Version { get; set; } = string.Empty;
+
+  
 }
 
 public class CatalogItemEntity
 {
     public Guid Id { get; set; }
+    public Guid VendorId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Version { get; set; } = string.Empty;
 
     public DateTimeOffset Created { get; set; }
+    public  CatalogItemDetails MapToResponse()
+    {
+        return new CatalogItemDetails
+        {
+            Id = Id,
+            Description =Description,
+            Name = Name,
+            Version = Version,
+        };
+    }
+}
+
+public interface ILookupVendors
+{
+    Task<bool> CheckIfVendorExistsAsync(Guid id);
 }
